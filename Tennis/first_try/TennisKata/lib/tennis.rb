07896a5da3ba288f1f76101008b5c13d6361cb78
unknown <joe@.statoil.net>
2010-11-25 00:52:43 +0100

    Finished advantage, starting on win

--- Tennis/first_try/TennisKata/lib/tennis.rb
+++ Tennis/first_try/TennisKata/lib/tennis.rb
@@ -20,8 +20,37 @@ class Tennis
   end
   
   def score
-    return "deuce" if @score_player_one >= 3 and @score_player_two >= 3
+    return "player one wins" if @score_player_one == 4 and @score_player_two == 0
+    return "player two wins" if @score_player_one == 0 and @score_player_two == 4
+    return "deuce" if both_players_have :forty and score_is_equal
+    return "advantage #{leading_player}" if both_players_have :forty and one_player_leads
     return pretty(@score_player_one) + " - " + pretty(@score_player_two)
+  end
+
+  def leading_player
+    return "player one" if player_one_leads
+    return "player two" if player_two_leads
+  end
+
+  def one_player_leads
+    player_one_leads or player_two_leads
+  end
+
+  def player_one_leads
+    @score_player_one > @score_player_two
+  end
+
+  def player_two_leads
+    @score_player_one < @score_player_two
+  end
+
+  def score_is_equal
+    @score_player_one == @score_player_two
+  end
+
+  def both_players_have score
+    score = 3 if score == :forty
+    @score_player_one >= score and @score_player_two >= score
   end
 
   def pretty balls
--- Tennis/first_try/TennisKata/spec/tennis_spec.rb
+++ Tennis/first_try/TennisKata/spec/tennis_spec.rb
@@ -35,8 +35,42 @@ describe Tennis do
     {
       {:player_one => 3, :player_two => 3} => "deuce",
       {:player_one => 4, :player_two => 4} => "deuce",
-#      {:player_one => 1, :player_two => 1} => "deuce",
-#      {:player_one => 2, :player_two => 1} => "deuce",
+      {:player_one => 5, :player_two => 5} => "deuce",
+      {:player_one => 6, :player_two => 6} => "deuce",
+      {:player_one => 7, :player_two => 7} => "deuce",
+    }.each do | balls, expected_result |
+      it "should return '#{expected_result}' when #{balls[:player_one]} - #{balls[:player_two]}" do
+        @tennis.play balls[:player_one], balls[:player_two]
+        @tennis.score.should eql expected_result
+      end
+    end
+  end
+
+  describe "advantage" do
+    {
+      {:player_one => 4, :player_two => 3} => "advantage player one",
+      {:player_one => 3, :player_two => 4} => "advantage player two",
+      {:player_one => 5, :player_two => 4} => "advantage player one",
+      {:player_one => 4, :player_two => 5} => "advantage player two",
+      {:player_one => 7, :player_two => 6} => "advantage player one",
+      {:player_one => 6, :player_two => 7} => "advantage player two",
+      
+    }.each do | balls, expected_result |
+      it "should return '#{expected_result}' when #{balls[:player_one]} - #{balls[:player_two]}" do
+        @tennis.play balls[:player_one], balls[:player_two]
+        @tennis.score.should eql expected_result
+      end
+    end
+  end
+
+  describe "win" do
+    {
+      {:player_one => 4, :player_two => 0} => "player one wins",
+      {:player_one => 0, :player_two => 4} => "player two wins",
+#      {:player_one => 5, :player_two => 4} => "advantage player one",
+#      {:player_one => 4, :player_two => 5} => "advantage player two",
+#      {:player_one => 7, :player_two => 6} => "advantage player one",
+#      {:player_one => 6, :player_two => 7} => "advantage player two",
     }.each do | balls, expected_result |
       it "should return '#{expected_result}' when #{balls[:player_one]} - #{balls[:player_two]}" do
         @tennis.play balls[:player_one], balls[:player_two]
