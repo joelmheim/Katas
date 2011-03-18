4639f5678c317c9e3196331d108aa16e5f1fcdba
Jørn Ølmheim <joe@dolphin-work.(none)>
2011-03-18 15:16:00 +0100

    A few new tests.

--- Tennis/javascript/TennisKata/spec/TennisSpec.js
+++ Tennis/javascript/TennisKata/spec/TennisSpec.js
@@ -1,15 +1,25 @@
 describe("Tennis", function() {
-  var tennis;
+  var tennis
 
   beforeEach(function() {
-    tennis = new Tennis();
-  });
+    tennis = new Tennis()
+  })
 
   describe("regular match", function() {
     
     it("should return love - love when score is 0 - 0", function() {
-      expect(tennis.score).toEqual("love - love");
-    });
+      expect(tennis.score()).toEqual('love - love')
+    })
 
-  });
-});
+    it("should return 'love - fifteen' when score is 0 - 1", function() {
+        tennis.scorePlayerOne()
+        expect(tennis.score()).toEqual('love - fifteen')
+    })
+
+    it("should return 'fifteen - love' when score is 1 - 0", function() {
+        tennis.scorePlayerTwo()
+        expect(tennis.score()).toEqual('fifteen - love')
+    })
+
+  })
+})
--- Tennis/javascript/TennisKata/src/Tennis.js
+++ Tennis/javascript/TennisKata/src/Tennis.js
@@ -1,3 +1,21 @@
 function Tennis() {
+    this.ballsPlayerOne = 0;
+    this.ballsPlayerTwo = 0;
 }
-Tennis.prototype.score = "love - love"
+
+Tennis.prototype.score = function() {
+    if (this.ballsPlayerOne == 1) {
+        return 'love - fifteen'
+    } else if (this.ballsPlayerTwo == 1) {
+        return 'fifteen - love'
+    }
+    return 'love - love'
+}
+
+Tennis.prototype.scorePlayerOne = function() {
+    this.ballsPlayerOne++
+}
+
+Tennis.prototype.scorePlayerTwo = function() {
+    this.ballsPlayerTwo++
+}
