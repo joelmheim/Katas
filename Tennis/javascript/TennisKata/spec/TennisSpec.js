3f83149ad2d32264ce5236f5d5f0bfb3ab123ba8
Jørn Ølmheim <joe@dolphin-work.(none)>
2011-03-22 14:06:10 +0100

    Trying to improve the code, but failing ...

--- Tennis/javascript/TennisKata/spec/TennisSpec.js
+++ Tennis/javascript/TennisKata/spec/TennisSpec.js
@@ -8,16 +8,17 @@ describe("Tennis", function() {
   describe("regular match", function() {
     
     it("should return love - love when score is 0 - 0", function() {
+      tennis.game(0,0)
       expect(tennis.score()).toEqual('love - love')
     })
 
     it("should return 'love - fifteen' when score is 0 - 1", function() {
-        tennis.scorePlayerOne()
+        tennis.game(0,1)
         expect(tennis.score()).toEqual('love - fifteen')
     })
 
     it("should return 'fifteen - love' when score is 1 - 0", function() {
-        tennis.scorePlayerTwo()
+        tennis.game(1,0)
         expect(tennis.score()).toEqual('fifteen - love')
     })
 
--- Tennis/javascript/TennisKata/src/Tennis.js
+++ Tennis/javascript/TennisKata/src/Tennis.js
@@ -1,21 +1,38 @@
 function Tennis() {
     this.ballsPlayerOne = 0;
     this.ballsPlayerTwo = 0;
-}
+    this.scores = ['love', 'fifteen']
 
-Tennis.prototype.score = function() {
-    if (this.ballsPlayerOne == 1) {
-        return 'love - fifteen'
-    } else if (this.ballsPlayerTwo == 1) {
-        return 'fifteen - love'
+    Number.prototype.times = function(obj, fun) {
+        alert(typeof fun)
+        if (this > 0) {
+            for(i=0; i<this; i++) {
+                obj.call(fun())
+            }
+        }
     }
-    return 'love - love'
-}
 
-Tennis.prototype.scorePlayerOne = function() {
-    this.ballsPlayerOne++
-}
+    Tennis.prototype.score = function() {
+        return this.scores[this.ballsPlayerOne] + ' - ' + this.scores[this.ballsPlayerTwo]
+    }
 
-Tennis.prototype.scorePlayerTwo = function() {
-    this.ballsPlayerTwo++
+    Tennis.prototype.scorePlayerOne = function() {
+        this.ballsPlayerOne++
+    }
+
+    Tennis.prototype.scorePlayerTwo = function() {
+        this.ballsPlayerTwo++
+    }
+
+    Tennis.prototype.game = function(playerOne, playerTwo) {
+//        playerOne.times(this, Tennis.prototype.scorePlayerOne())
+//    playerTwo.times(this.scorePlayerTwo())
+        for(i=0; i<playerOne; i++) {
+          this.scorePlayerOne()
+        }
+        for(j=0; j<playerTwo; j++) {
+            this.scorePlayerTwo()
+        }
+    }
+
 }
