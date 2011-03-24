b84550f358d7ec659c3baae2c8f35c2226d22e0d
Jørn Ølmheim <joe@dolphin-work.(none)>
2011-03-24 23:13:09 +0100

    Breakthrough. Found out how to  describe tests more efficiently.

--- Tennis/javascript/TennisKata/spec/TennisSpec.js
+++ Tennis/javascript/TennisKata/spec/TennisSpec.js
@@ -1,26 +1,76 @@
 describe("Tennis", function() {
-  var tennis
+  var tennis;
 
   beforeEach(function() {
-    tennis = new Tennis()
-  })
+    tennis = new Tennis();
+  });
 
   describe("regular match", function() {
-    
-    it("should return love - love when score is 0 - 0", function() {
-      tennis.game(0,0)
-      expect(tennis.score()).toEqual('love - love')
-    })
+      [
+          {playerOne: 0, playerTwo: 0, result: 'love - love' },
+          {playerOne: 0, playerTwo: 1, result: 'love - fifteen' },
+          {playerOne: 1, playerTwo: 0, result: 'fifteen - love' },
+          {playerOne: 1, playerTwo: 1, result: 'fifteen - fifteen' },
+          {playerOne: 1, playerTwo: 2, result: 'fifteen - twenty' },
+          {playerOne: 2, playerTwo: 1, result: 'twenty - fifteen' },
+          {playerOne: 2, playerTwo: 2, result: 'twenty - twenty' },
+          {playerOne: 2, playerTwo: 3, result: 'twenty - thirty' },
+          {playerOne: 3, playerTwo: 2, result: 'thirty - twenty' },
+          {playerOne: 3, playerTwo: 3, result: 'thirty - thirty'},
+          {playerOne: 3, playerTwo: 4, result: 'thirty - forty'},
+          {playerOne: 4, playerTwo: 3, result: 'forty - thirty'}
+      ].forEach(function(testGame) {it ("should return " + testGame.result + " when score is " + testGame.playerOne + " - " + testGame.playerTwo, function() {
+            tennis.game(testGame.playerOne, testGame.playerTwo);
+            expect(tennis.score()).toBe(testGame.result);
+          });
+      });
+  });
 
-    it("should return 'love - fifteen' when score is 0 - 1", function() {
-        tennis.game(0,1)
-        expect(tennis.score()).toEqual('love - fifteen')
-    })
+  describe("deuce", function() {
+      [
+         {playerOne: 4, playerTwo: 4, result: 'deuce'},
+         {playerOne: 5, playerTwo: 5, result: 'deuce'},
+         {playerOne: 6, playerTwo: 6, result: 'deuce'},
+         {playerOne: 7, playerTwo: 7, result: 'deuce'},
+         {playerOne: 8, playerTwo: 8, result: 'deuce'},
+         {playerOne: 9, playerTwo: 9, result: 'deuce'},
+         {playerOne: 10, playerTwo: 10, result: 'deuce'}
+      ].forEach(function(testGame) {it ("should return " + testGame.result + " when score is " + testGame.playerOne + " - " + testGame.playerTwo, function() {
+            tennis.game(testGame.playerOne, testGame.playerTwo);
+            expect(tennis.score()).toBe(testGame.result);
+          });
+      });
+  });
 
-    it("should return 'fifteen - love' when score is 1 - 0", function() {
-        tennis.game(1,0)
-        expect(tennis.score()).toEqual('fifteen - love')
-    })
+  describe("deuce", function() {
+      [
+           {playerOne: 4, playerTwo: 4, result: 'deuce'},
+           {playerOne: 5, playerTwo: 5, result: 'deuce'},
+           {playerOne: 6, playerTwo: 6, result: 'deuce'},
+           {playerOne: 7, playerTwo: 7, result: 'deuce'},
+           {playerOne: 8, playerTwo: 8, result: 'deuce'},
+           {playerOne: 9, playerTwo: 9, result: 'deuce'},
+           {playerOne: 10, playerTwo: 10, result: 'deuce'}
+      ].forEach(function(testGame) {it ("should return " + testGame.result + " when score is " + testGame.playerOne + " - " + testGame.playerTwo, function() {
+              tennis.game(testGame.playerOne, testGame.playerTwo);
+              expect(tennis.score()).toBe(testGame.result);
+          });
+      });
+    });
 
-  })
-})
+    describe("advantage", function() {
+        [
+             {playerOne: 5, playerTwo: 4, result: 'advantage player one'} //,
+             //{playerOne: 4, playerTwo: 5, result: 'advantage player two'},
+             //{playerOne: 6, playerTwo: 5, result: 'advantage player one'},
+             //{playerOne: 5, playerTwo: 6, result: 'advantage player two'},
+             //{playerOne: 7, playerTwo: 6, result: 'advantage player one'},
+             //{playerOne: 6, playerTwo: 7, result: 'advantage player two'},
+             //{playerOne: 10, playerTwo: 10, result: 'deuce'}
+        ].forEach(function(testGame) {it ("should return " + testGame.result + " when score is " + testGame.playerOne + " - " + testGame.playerTwo, function() {
+                tennis.game(testGame.playerOne, testGame.playerTwo);
+                expect(tennis.score()).toBe(testGame.result);
+            });
+        });
+      });
+});
--- Tennis/javascript/TennisKata/src/Tennis.js
+++ Tennis/javascript/TennisKata/src/Tennis.js
@@ -1,38 +1,33 @@
 function Tennis() {
     this.ballsPlayerOne = 0;
     this.ballsPlayerTwo = 0;
-    this.scores = ['love', 'fifteen']
+    this.scores = ['love', 'fifteen', 'twenty', 'thirty', 'forty'];
 
-    Number.prototype.times = function(obj, fun) {
-        alert(typeof fun)
-        if (this > 0) {
-            for(i=0; i<this; i++) {
-                obj.call(fun())
-            }
+    Tennis.prototype.scoreIsEqual = function() {
+        return this.ballsPlayerOne === this.ballsPlayerTwo;
+    }
+
+    Tennis.prototype.winningPlayer = function() {
+        if (this.ballsPlayerOne > this.ballsPlayerTwo) {
+            return 'player one';
+        } else if (this.ballsPlayerOne < this.ballsPlayerTwo) {
+            return 'player two';
         }
     }
 
     Tennis.prototype.score = function() {
-        return this.scores[this.ballsPlayerOne] + ' - ' + this.scores[this.ballsPlayerTwo]
-    }
-
-    Tennis.prototype.scorePlayerOne = function() {
-        this.ballsPlayerOne++
-    }
-
-    Tennis.prototype.scorePlayerTwo = function() {
-        this.ballsPlayerTwo++
+        if (this.ballsPlayerOne >= 4 && this.ballsPlayerTwo >= 4 && this.scoreIsEqual()) {
+            return 'deuce';
+        } else if (this.ballsPlayerOne >= 4 && this.ballsPlayerTwo >= 4) {
+            return 'advantage ' + this.winningPlayer();
+        } else {
+            return this.scores[this.ballsPlayerOne] + ' - ' + this.scores[this.ballsPlayerTwo];
+        }
     }
 
     Tennis.prototype.game = function(playerOne, playerTwo) {
-//        playerOne.times(this, Tennis.prototype.scorePlayerOne())
-//    playerTwo.times(this.scorePlayerTwo())
-        for(i=0; i<playerOne; i++) {
-          this.scorePlayerOne()
-        }
-        for(j=0; j<playerTwo; j++) {
-            this.scorePlayerTwo()
-        }
+        this.ballsPlayerOne = playerOne;
+        this.ballsPlayerTwo = playerTwo;
     }
 
 }
