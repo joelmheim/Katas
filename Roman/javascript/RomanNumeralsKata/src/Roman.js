30af99a0c202d620846cca06896d8fd8817ab5e4
Jørn Ølmheim <jorn@olmheim.com>
2011-08-01 10:45:29 +0200

    Added still more tests. Found out that it doesn't work for 3998 and 3999. Possibly due to Jasmine recursion limits...

--- Roman/javascript/RomanNumeralsKata/src/Roman.js
+++ Roman/javascript/RomanNumeralsKata/src/Roman.js
@@ -1,6 +1,6 @@
 function makeRomanNumerals() {
 	var initialArabic, 
-	    tokens = { 1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M" }; 
+	    tokens = { 1000: "M", 900: "CM", 500: "D", 400: "CD", 100: "C", 90: "XC", 50: "L", 40: "XL", 10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I" }; 
 	
 	function findToken(arabic, remainder) {
 		var result, currentToken = tokens[arabic];
